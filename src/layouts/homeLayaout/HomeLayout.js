829feddad231c61ac7d448efa951e3c21ced28bd
Miguel Cardenas <miguel.cardenas@zemoga.com>
2021-06-25 18:11:17 -0500

    Add logo

--- src/layouts/homeLayaout/HomeLayout.js
+++ src/layouts/homeLayaout/HomeLayout.js
@@ -3,15 +3,17 @@ import PropTypes from 'prop-types';
 import { useLocation } from 'react-router-dom';
 
 import { NAV_LINKS } from '../../contants';
-import NavBar from '../../components/navBar';
-
 import { getActiveNavLink } from '../../helpers';
+
+import Logo from '../../components/logo';
+import NavBar from '../../components/navBar';
 
 const HomeLayout = ({ children }) => {
   const { pathname } = useLocation();
 
   return (
     <section>
+      <Logo />
       <NavBar links={getActiveNavLink(NAV_LINKS, pathname)} />
       hi
       {children}
--- src/layouts/loginLayout/LoginLayout.js
+++ src/layouts/loginLayout/LoginLayout.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import Logo from '../../components/logo';
+
 function SignInLayout({ children }) {
   return (
     <div>
+      <section className="lg:w-2/4 lg:p-12 lg:p-6 p-20 bg-gray-100 bg-signin-image bg-cover bg-center">
+        <Logo />
+      </section>
       <section>{children}</section>
     </div>
   );
